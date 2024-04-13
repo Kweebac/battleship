a62d79ea7ac827a8c9a39237f98cdb250830ede1
Kweebac <06.amarshall@gmail.com>
2024-04-13 19:43:14 +0100

    Made AI smarter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import PlayerBoard from "./components/PlayerBoard";
 import ComputerBoard from "./components/ComputerBoard";
 
 export default function App() {
   const [player, setPlayer] = useState([
-    ["", "", "", "", "", "ship", "ship", "", "", ""],
+    ["", "miss", "", "", "miss", "ship", "ship", "miss", "", ""],
     ["", "ship", "", "", "", "", "", "", "", ""],
-    ["", "ship", "", "", "", "", "", "", "", ""],
+    ["", "ship", "", "", "", "miss", "", "", "", ""],
     ["", "ship", "", "", "", "ship", "", "", "", ""],
     ["", "ship", "", "", "", "ship", "", "", "", ""],
     ["", "ship", "", "", "", "ship", "", "", "", ""],
+    ["", "miss", "", "", "", "miss", "", "", "", ""],
     ["", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", ""],
-    ["", "ship", "ship", "", "", "", "", "", "", ""],
-    ["", "", "", "", "ship", "ship", "ship", "ship", "", ""],
+    ["miss", "ship", "ship", "miss", "", "", "", "", "", ""],
+    ["", "", "", "miss", "ship", "ship", "ship", "ship", "miss", ""],
   ]);
   const [computer, setComputer] = useState([
     ["", "", "", "", "", "ship", "ship", "", "", ""],
@@ -27,18 +27,94 @@ export default function App() {
     ["", "ship", "ship", "", "", "", "", "", "", ""],
     ["", "", "", "", "ship", "ship", "ship", "ship", "", ""],
   ]);
+  const computerAttackQueue = useRef<
+    {
+      coords: [number, number];
+      axis: "y" | "x";
+    }[]
+  >([]);
 
   const computerAttack = useCallback(() => {
     const playerCopy = JSON.parse(JSON.stringify(player));
-    while (true) {
-      const row = Math.floor(Math.random() * 10);
-      const col = Math.floor(Math.random() * 10);
-      if (playerCopy[row][col] === "ship") {
-        playerCopy[row][col] = "hit";
-        break;
-      } else if (playerCopy[row][col] === "") {
-        playerCopy[row][col] = "miss";
-        break;
+
+    if (computerAttackQueue.current.length > 0) {
+      while (true) {
+        const latestQueueItem = computerAttackQueue.current.pop();
+        const [row, col] = latestQueueItem?.coords;
+        const axis = latestQueueItem?.axis;
+
+        if (playerCopy[row][col] === "ship") {
+          playerCopy[row][col] = "hit";
+
+          if (axis === "y") {
+            // delete all x axis in queue
+            computerAttackQueue.current = computerAttackQueue.current.filter(
+              (queueItem) => queueItem.axis === "y",
+            );
+
+            // add y axis cell on either side of current cell
+            computerAttackQueue.current.push({
+              coords: [row + 1, col],
+              axis: "y",
+            });
+            computerAttackQueue.current.push({
+              coords: [row - 1, col],
+              axis: "y",
+            });
+          } else if (axis === "x") {
+            // delete all y axis in queue
+            computerAttackQueue.current = computerAttackQueue.current.filter(
+              (queueItem) => queueItem.axis === "x",
+            );
+
+            // add x axis cell on either side of current cell
+            computerAttackQueue.current.push({
+              coords: [row, col + 1],
+              axis: "x",
+            });
+            computerAttackQueue.current.push({
+              coords: [row, col - 1],
+              axis: "x",
+            });
+          }
+
+          break;
+        } else if (playerCopy[row][col] === "") {
+          playerCopy[row][col] = "miss";
+          break;
+        }
+      }
+    } else {
+      while (true) {
+        const row = Math.floor(Math.random() * 10);
+        const col = Math.floor(Math.random() * 10);
+
+        if (playerCopy[row][col] === "ship") {
+          const computerAddAttackToQueue = (
+            row: number,
+            col: number,
+            axis: "y" | "x",
+          ) => {
+            if (row < 0 || row >= 10 || col < 0 || col >= 10) return;
+            computerAttackQueue.current.push({
+              coords: [row, col],
+              axis,
+            });
+          };
+
+          playerCopy[row][col] = "hit";
+
+          computerAddAttackToQueue(row + 1, col, "y");
+          computerAddAttackToQueue(row - 1, col, "y");
+          computerAddAttackToQueue(row, col + 1, "x");
+          computerAddAttackToQueue(row, col - 1, "x");
+
+          break;
+        } else if (playerCopy[row][col] === "") {
+          playerCopy[row][col] = "miss";
+
+          break;
+        }
       }
     }
 
@@ -55,16 +131,14 @@ export default function App() {
     [computer, setComputer],
   );
 
-  const computerBoardProps = {
-    computer,
-    computerAttack,
-    playerAttack,
-  };
-
   return (
-    <main className="grid h-screen place-content-center gap-8 md:grid-flow-col md:gap-[5rem] lg:gap-[7.5rem] xl:gap-[10rem] 2xl:gap-[12.5rem]">
+    <main className="grid h-screen place-content-center gap-8 md:grid-flow-col md:gap-[5rem] lg:gap-[7.5rem] xl:gap-[10rem]">
       <PlayerBoard player={player} />
-      <ComputerBoard {...computerBoardProps} />
+      <ComputerBoard
+        computer={computer}
+        computerAttack={computerAttack}
+        playerAttack={playerAttack}
+      />
     </main>
   );
 }
