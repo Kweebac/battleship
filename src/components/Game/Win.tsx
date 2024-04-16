9bb6e23fd5b49c4337d126cf7f8aa3d190c430d8
Kweebac <06.amarshall@gmail.com>
2024-04-16 14:14:18 +0100

    Changed win color scheme

--- src/components/Game/Win.tsx
+++ src/components/Game/Win.tsx
@@ -6,11 +6,11 @@ type WinProps = {
 export default function Win({ winner, reset }: WinProps) {
   return (
     <section className="absolute grid h-screen w-screen place-content-center backdrop-blur-sm">
-      <div className="grid w-screen justify-items-center gap-8 bg-yellow-50 p-12">
+      <div className="grid w-screen justify-items-center gap-8 bg-blue-300 p-12">
         <h1 className="text-6xl">{winner + " won!"}</h1>
         <button
           onClick={reset}
-          className="h-12 w-36 rounded-lg bg-[wheat] text-lg"
+          className="h-12 w-36 rounded-lg bg-blue-500 text-lg"
         >
           Play again
         </button>
